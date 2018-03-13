14a79d5aaac89b75f29c6672c92d215b3031324e
Jeremy Shimko <jeremy.shimko@gmail.com>
2018-03-13 13:45:06 -0400

    clean up install script

--- install.sh
+++ install.sh
@@ -1,24 +1,25 @@
 #!/bin/sh
 
-HELM_FILENAME="helm-v${HELM_VERSION}-linux-amd64.tar.gz"
-
-apk add --update ca-certificates git python
+apk add --update bash ca-certificates git python
 apk add --update -t deps curl make py-pip
-apk add bash
 
+# install kubectl
 curl -L https://storage.googleapis.com/kubernetes-release/release/v${KUBECTL_VERSION}/bin/linux/amd64/kubectl -o /usr/local/bin/kubectl
 chmod +x /usr/local/bin/kubectl
+
+# install Helm
+HELM_FILENAME="helm-v${HELM_VERSION}-linux-amd64.tar.gz"
 curl -L http://storage.googleapis.com/kubernetes-helm/${HELM_FILENAME} -o /tmp/${HELM_FILENAME}
 tar -zxvf /tmp/${HELM_FILENAME} -C /tmp
 mv /tmp/linux-amd64/helm /usr/local/bin/helm
 
-# Setup Helm so that it will work with helm dep commands
+# initialize Helm client
 helm init -c
 
 # install S3 plugin for Helm
 helm plugin install https://github.com/hypnoglow/helm-s3.git --version $HELM_S3_PLUGIN_VERSION
 
-# install YAML linter
+# install YAML tools
 pip install yamllint yq
 
 # cleanup
