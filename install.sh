317bff2c9080ce48ea09804cef88fe339430b457
Jeremy Shimko <jeremy.shimko@gmail.com>
2020-02-17 09:02:04 -0500

    check sha hash for aws-authenticator

--- install.sh
+++ install.sh
@@ -15,8 +15,10 @@ chmod +x /usr/local/bin/kubectl
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | sh
 
 # aws-iam-authenticator
-# https://github.com/kubernetes-sigs/aws-iam-authenticator
+# https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html
 curl -o aws-iam-authenticator https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator.sha256
+openssl sha1 -sha256 aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mv aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
 aws-iam-authenticator help
