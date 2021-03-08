508a184547d948bde50e639fdda5bf5d7162d5cc
Jeremy Shimko <jeremy.shimko@gmail.com>
2021-03-08 10:45:24 -0500

    bump tool versions

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,10 @@
-FROM alpine:3.12
+FROM alpine:3.13
 
 # https://aur.archlinux.org/packages/kubectl-bin/
-ENV KUBECTL_VERSION "1.19.2"
+ENV KUBECTL_VERSION "1.20.4"
 
 # https://github.com/hypnoglow/helm-s3
-ENV HELM_S3_PLUGIN_VERSION "0.9.2"
+ENV HELM_S3_PLUGIN_VERSION "0.10.0"
 
 # set some defaults
 ENV AWS_DEFAULT_REGION "us-east-1"
--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-apk add --update py-pip curl make openssl groff
+apk add --update py3-pip curl make openssl groff
 
 # install kubectl
 # https://aur.archlinux.org/packages/kubectl-bin/
@@ -18,8 +18,8 @@ helm plugin install https://github.com/hypnoglow/helm-s3.git --version $HELM_S3_
 
 # aws-iam-authenticator
 # https://docs.aws.amazon.com/eks/latest/userguide/install-aws-iam-authenticator.html
-curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/aws-iam-authenticator
-curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.17.9/2020-08-04/bin/linux/amd64/aws-iam-authenticator.sha256
+curl -o aws-iam-authenticator https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator
+curl -o aws-iam-authenticator.sha256 https://amazon-eks.s3.us-west-2.amazonaws.com/1.19.6/2021-01-05/bin/linux/amd64/aws-iam-authenticator.sha256
 openssl sha1 -sha256 aws-iam-authenticator
 chmod +x ./aws-iam-authenticator
 mv aws-iam-authenticator /usr/local/bin/aws-iam-authenticator
